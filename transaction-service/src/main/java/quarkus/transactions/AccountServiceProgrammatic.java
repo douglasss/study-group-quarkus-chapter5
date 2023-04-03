1abb96af87ed27cf677390c2365db8cbb044f132
Douglas Silva <douglas.souza.93.ds@gmail.com>
2023-04-03 14:21:20 -0300

    [5.2.2] Create the AccountServiceProgrammatic interface

---
+++ transaction-service/src/main/java/quarkus/transactions/AccountServiceProgrammatic.java
@@ -0,0 +1,35 @@
+package quarkus.transactions;
+
+import java.math.BigDecimal;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+
+// Same as 'AccountService', only removing the @RegisterRestClient annotation.
+// The same interface could be used for CDI and programmatic API
+// But it’s important to show that @RegisterRestClient is not required for programmatic API usage.
+@Path("/accounts")
+@RegisterRestClient(configKey = "account-service")
+@Produces(MediaType.APPLICATION_JSON)
+public interface AccountServiceProgrammatic {
+
+    @GET
+    @Path("/{accountNumber}/balance")
+    BigDecimal getBalance(
+            @PathParam("accountNumber")
+            Long accountNumber);
+
+    @POST
+    @Path("/{accountNumber}/transaction")
+    void transact(
+            @PathParam("accountNumber")
+            Long accountNumber, BigDecimal amount);
+
+}
