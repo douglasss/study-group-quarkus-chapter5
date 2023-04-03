f5616384d764506e0cc13b12d59d86c9a91766f1
Douglas Silva <douglas.souza.93.ds@gmail.com>
2023-04-03 12:49:11 -0300

    [5.2] Create AccountService

---
+++ transaction-service/src/main/java/quarkus/transactions/AccountService.java
@@ -0,0 +1,31 @@
+package quarkus.transactions;
+
+import java.math.BigDecimal;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/account") // Defines the path of the service, excluding the base URL portion
+@RegisterRestClient // Indicates that the interface should have a CDI bean created that can be injected into classes
+@Produces(MediaType.APPLICATION_JSON) // Sets all methods of the service to return JSON
+public interface AccountService {
+
+    // Method for retrieving the account balance, with HTTP method and Path annotations
+    @GET
+    @Path("/{accountNumber}/balance")
+    BigDecimal getBalance(
+            @PathParam("accountNumber")
+            Long accountNumber);
+
+    // Method for transacting on an account, with HTTP method and Path annotations
+    @POST
+    @Path("/{accountNumber}/transaction")
+    void transact(Long accountNumber, BigDecimal amount);
+
+}
