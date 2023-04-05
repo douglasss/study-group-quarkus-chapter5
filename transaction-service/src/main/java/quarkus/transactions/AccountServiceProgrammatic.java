ec7d6d30d862ac9ad3ed47d053f7234d9795d770
Douglas Silva <douglas.souza.93.ds@gmail.com>
2023-04-05 12:50:08 -0300

    [5.2.2] Builder API

---
+++ transaction-service/src/main/java/quarkus/transactions/AccountServiceProgrammatic.java
@@ -0,0 +1,28 @@
+package quarkus.transactions;
+
+import java.math.BigDecimal;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/accounts")
+@Produces(MediaType.APPLICATION_JSON)
+public interface AccountServiceProgrammatic {
+
+    @GET
+    @Path("/{accountNumber}/balance")
+    BigDecimal getBalance(
+            @PathParam("accountNumber")
+            Long accountNumber);
+
+    @POST
+    @Path("/{accountNumber}/transaction")
+    void transact(
+            @PathParam("accountNumber")
+            Long accountNumber, BigDecimal amount);
+
+}
--- transaction-service/src/main/java/quarkus/transactions/TransactionResource.java
+++ transaction-service/src/main/java/quarkus/transactions/TransactionResource.java
@@ -1,6 +1,9 @@
 package quarkus.transactions;
 
 import java.math.BigDecimal;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import javax.inject.Inject;
 import javax.ws.rs.POST;
@@ -8,6 +11,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.rest.client.RestClientBuilder;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/transactions")
@@ -23,6 +27,23 @@ public class TransactionResource {
             @PathParam("accountNumber")
             Long accountNumber, BigDecimal amount) {
         accountService.transact(accountNumber, amount);
+        return Response.ok().build();
+    }
+
+    @POST
+    @Path("/api/{accountNumber}")
+    public Response newTransactionApi(
+            @PathParam("accountNumber")
+            Long accountNumber, BigDecimal amount) throws MalformedURLException {
+
+        AccountServiceProgrammatic service = RestClientBuilder.newBuilder()
+                .baseUrl(new URL("http://localhost:8080"))
+                .connectTimeout(500, TimeUnit.MILLISECONDS)
+                .readTimeout(1200, TimeUnit.MILLISECONDS)
+                .build(AccountServiceProgrammatic.class);
+
+        service.transact(accountNumber, amount);
+
         return Response.ok().build();
     }
 
