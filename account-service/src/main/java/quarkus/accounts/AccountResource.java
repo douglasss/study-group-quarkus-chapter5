7efafe15a3d89a8caa3676a00582fd226bc0e4fc
Douglas Silva <douglas.souza.93.ds@gmail.com>
2023-04-05 08:52:47 -0300

    Initial code from chapter 5

--- account-service/src/main/java/quarkus/accounts/AccountResource.java
+++ account-service/src/main/java/quarkus/accounts/AccountResource.java
@@ -8,10 +8,8 @@ import javax.json.Json;
 import javax.json.JsonObjectBuilder;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,7 +18,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
